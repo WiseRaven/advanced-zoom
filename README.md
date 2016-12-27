e03ec500053232de0723c1c7a95666ba02055d63
Andrea Cognini <info@heavybeard.it>
2016-12-27 11:12:07 +0100

    new name advanced-zoom, ready to bower

--- README.md
+++ README.md
@@ -1,31 +1,31 @@
-# ZOOM-VANILLA.JS
+# ADVANCED ZOOM
 
 A simple library for image zooming; as seen on
 [Medium](https://medium.com/designing-medium/image-zoom-on-medium-24d146fc0c20).
 
 This library is a fork of the [original jQuery plugin by
 fat](https://github.com/fat/zoom.js). This version does not depend on jQuery.
-I'm also trying not to modify the original code too much so that consuming
-upstream changes (if any) becomes easier.
+Originally forked from [zoom-vanilla.js](https://github.com/spinningarrow/zoom-vanilla.js)
 
-I added new feature for video zooming that works like the image zooming.
+I added new feature for `video` and `picture` zooming that works like the image zooming.
+Looking for demo and try all supports
 
 ### Demo
 http://heavybeard.github.io/advanced-zoom/dist/
 
 ### How
 
-1. Link the zoom-vanilla.js and zoom.css files to your site or application.
+1. Link the advanced-zoom.js and advanced-zoom.css files to your site or application.
 
   ```html
-  <link href="asset/css/zoom.min.css" rel="stylesheet">
-  <script src="asset/js/zoom-vanilla.min.js"></script>
+  <link href="asset/css/advanced-zoom.min.css" rel="stylesheet">
+  <script src="asset/js/advanced-zoom.min.js"></script>
   ```
 
-2. Add a `data-action="zoom"` attribute to the images or the videos you want to make zoomable. For example:
+2. Add a `data-zoom="zoom"` attribute to the images or the videos you want to make zoomable. For example:
 
   ```html
-  <img src="asset/img/forest.jpg" data-action="zoom">
+  <img src="asset/img/forest.jpg" data-zoom="zoom">
   ```
   
   ```html
@@ -33,12 +33,12 @@ http://heavybeard.github.io/advanced-zoom/dist/
     <!--[if IE 9]><video style="display: none;"><![endif]-->
     <source srcset="asset/img/city.webp" type="image/webp">
     <!--[if IE 9]></video><![endif]-->
-    <img src="asset/img/city.jpg" data-action="zoom">
+    <img src="asset/img/city.jpg" data-zoom="zoom">
   </picture>
   ```
   
   ```html
-  <video data-action="zoom" width="320">
+  <video data-zoom="zoom" width="320">
     <source src="asset/video/storm.webm" type="video/webm">
     <source src="asset/video/storm.ogv" type="video/ogv">
     <source src="asset/video/storm.mp4" type="video/mp4">
@@ -53,19 +53,17 @@ If you hold your meta key (`⌘` or `Ctrl`) it will open in a new tab.
 If you add the new data property `data-original="path/to/image"` it will open a different image on the new tab, for example the full size image.
 
 ```html
-<img src="asset/img/city.jpg" data-action="zoom" data-original="asset/img/city.jpg">
+<img src="asset/img/city.jpg" data-zoom="zoom" data-original="asset/img/city.jpg">
 ```
 
 ### Where
 
-zoom-vanilla.js should (in theory) work in all modern browsers, in IE9 too. If not, create an issue! Thanks!
-
+advanced-zoom.js should (in theory) work in all modern browsers, in IE9 too. If not, create an issue! Thanks!
 
 ### Who
 
-This version by [@spinningarrow][] and [@heavybeard][].
+This version by [@heavybeard][], forked originally from [@spinningarrow][].
 
-Originally written by <a href="//twitter.com/fat">@fat</a>, made better by you.
+Originally written by <a href="//twitter.com/fat">@fat</a>
 
-[@spinningarrow]: https://github.com/spinningarrow
 [@heavybeard]: https://github.com/heavybeard
--- source/script/advanced-zoom.js
+++ source/script/advanced-zoom.js
@@ -1,10 +1,10 @@
 /**
- * ZOOM VANILLA
- * @description Best way to zoom a media
- * @see https://github.com/spinningarrow/zoom-vanilla.js
+ * ADVANCED ZOOM
+ * @description Zoom on img, picture and video source like Medium
+ * @see http://heavybeard.github.io/advanced-zoom/dist/
  */
 
-+function() {
++ function () {
     "use strict";
 
     /** Global functions */
@@ -59,37 +59,37 @@
 
 
     /**
-     * ZOOMSERVICE
-     * @description Create the ZoomService object
+     * ADVANCEDZOOMSERVICE
+     * @description Create the AdvancedZoomService object
      */
-    function ZoomService() {
-        this._activeZoom = this._initialScrollPosition = this._initialTouchPosition = this._touchMoveListener = null;
+    function AdvancedZoomService() {
+        this._activeAdvancedZoom = this._initialScrollPosition = this._initialTouchPosition = this._touchMoveListener = null;
         this._window = window;
         this._document = document;
         this._body = document.body;
     }
 
     /**
-     * ZOOMSERVICE LISTEN
-     * @description Add _zoom on clicked data-zoom DOM element
+     * ADVANCEDZOOMSERVICE LISTEN
+     * @description Add _advancedZoom on clicked data-zoom DOM element
      */
-    ZoomService.prototype.listen = function() {
-        document.body.addEventListener('click', function(event) {
+    AdvancedZoomService.prototype.listen = function () {
+        document.body.addEventListener('click', function (event) {
             if (event.target.getAttribute('data-zoom') === 'zoom')
-                this._zoom(event);
+                this._advancedZoom(event);
         }.bind(this));
     };
 
     /**
-     * ZOOMSERVICE _ZOOM
-     * @description Add the _zoom
+     * ADVANCEDZOOMSERVICE _ADVANCEDZOOM
+     * @description Add the _advancedZoom
      * @param object e The Event object
      * @return For stopping
      */
-    ZoomService.prototype._zoom = function(e) {
+    AdvancedZoomService.prototype._advancedZoom = function (e) {
         var target = e.target;
 
-        /** STOP _ZOOM */
+        /** STOP _ADVANCEDZOOM */
         /** if is not IMG or VIDEO tag */
         if (!target || target.tagName !== 'IMG' && target.tagName !== 'VIDEO' && target.tagName !== 'PICTURE')
             return;
@@ -100,13 +100,13 @@
         if (e.metaKey || e.ctrlKey)
             return window.open((e.target.getAttribute('data-zoom-original') || e.target.currentSrc || e.target.src), '_blank');
         /** if width is bigger than viewport width */
-        if (target.width >= (window.innerWidth - Zoom.OFFSET))
+        if (target.width >= (window.innerWidth - AdvancedZoom.OFFSET))
             return;
 
-        /** START _ZOOM */
-        this._activeZoomClose(true);
-        this._activeZoom = new Zoom(target);
-        this._activeZoom.zoomMedia(target.tagName);
+        /** START _ADVANCEDZOOM */
+        this._activeAdvancedZoomClose(true);
+        this._activeAdvancedZoom = new AdvancedZoom(target);
+        this._activeAdvancedZoom.advancedZoomMedia(target.tagName);
         /** Add current functions to global functions on element event listener */
         scrollHandlerFn = this._scrollHandler.bind(this);
         clickHandlerFn = this._clickHandler.bind(this);
@@ -121,36 +121,36 @@
     };
 
     /**
-     * ZOOMSERVICE _ACTIVEZOOMCLOSE
-     * @description Reset the _zoom
+     * ADVANCEDZOOMSERVICE _ACTIVEADVANCEDZOOMCLOSE
+     * @description Reset the _advancedZoom
      * @param bool forceDispose Set true for completely remove
      * @return For stopping
      */
-    ZoomService.prototype._activeZoomClose = function(forceDispose) {
-        /** Stop closing if _zoom is not active */
-        if (!this._activeZoom)
+    AdvancedZoomService.prototype._activeAdvancedZoomClose = function (forceDispose) {
+        /** Stop closing if _advancedZoom is not active */
+        if (!this._activeAdvancedZoom)
             return;
 
         /** Force the remove */
         if (forceDispose)
-            this._activeZoom.dispose();
+            this._activeAdvancedZoom.dispose();
         else
-            this._activeZoom.close();
+            this._activeAdvancedZoom.close();
 
         /** Remove current functions to global functions on element event listener */
         this._window.removeEventListener('scroll', scrollHandlerFn);
         this._document.removeEventListener('click', clickHandlerFn);
         this._document.removeEventListener('keyup', keyHandlerFn);
         this._document.removeEventListener('touchstart', touchStartFn);
-        this._activeZoom = null;
+        this._activeAdvancedZoom = null;
     };
 
     /**
-     * ZOOMSERVICE _SCROLLHANDLER
+     * ADVANCEDZOOMSERVICE _SCROLLHANDLER
      * @description Close zoom on scroll
      * @param object e The Event object
      */
-    ZoomService.prototype._scrollHandler = function(e) {
+    AdvancedZoomService.prototype._scrollHandler = function (e) {
         var deltaY;
 
         if (this._initialScrollPosition === null)
@@ -159,89 +159,89 @@
         deltaY = this._initialScrollPosition - window.scrollY;
 
         if (Math.abs(deltaY) >= 40)
-            this._activeZoomClose();
+            this._activeAdvancedZoomClose();
     };
 
     /**
-     * ZOOMSERVICE _KEYHANDLER
+     * ADVANCEDZOOMSERVICE _KEYHANDLER
      * @description Close zoom on Escape key press
      * @param object e The Event object
      */
-    ZoomService.prototype._keyHandler = function(e) {
+    AdvancedZoomService.prototype._keyHandler = function (e) {
         if (e.keyCode == 27)
-            this._activeZoomClose();
+            this._activeAdvancedZoomClose();
     };
 
     /**
-     * ZOOMSERVICE _CLICKHANLDER
+     * ADVANCEDZOOMSERVICE _CLICKHANLDER
      * @description Close zoom on click
      * @param object e The Event object
      */
-    ZoomService.prototype._clickHandler = function(e) {
+    AdvancedZoomService.prototype._clickHandler = function (e) {
         e.stopPropagation();
         e.preventDefault();
-        this._activeZoomClose();
+        this._activeAdvancedZoomClose();
     };
 
     /**
-     * ZOOMSERVICE _TOUCHSTART
+     * ADVANCEDZOOMSERVICE _TOUCHSTART
      * @description Add touchmove event listen
      * @param object e The Event object
      */
-    ZoomService.prototype._touchStart = function(e) {
+    AdvancedZoomService.prototype._touchStart = function (e) {
         this._initialTouchPosition = e.touches[0].pageY;
         touchMoveFn = this._touchMove.bind(this);
         e.target.addEventListener('touchmove', touchMoveFn);
     };
 
     /**
-     * ZOOMSERVICE _TOUCHMOVE
+     * ADVANCEDZOOMSERVICE _TOUCHMOVE
      * @description Close zoom on touch move
      * @param object e The Event object
      */
-    ZoomService.prototype._touchMove = function(e) {
+    AdvancedZoomService.prototype._touchMove = function (e) {
         if (Math.abs(e.touches[0].pageY - this._initialTouchPosition) > 10) {
-            this._activeZoomClose();
+            this._activeAdvancedZoomClose();
             e.target.removeEventListener('touchmove', touchMoveFn);
         }
     };
 
 
     /**
-     * ZOOM
-     * @description The Zoom object
+     * ADVANCEDZOOM
+     * @description The AdvancedZoom object
      * @param DOM media The media element
      */
-    function Zoom(media) {
+    function AdvancedZoom(media) {
         this._fullHeight = this._fullWidth = this._overlay = this._targetMediaWrap = null;
         this._targetMedia = media;
         this._body = document.body;
     }
 
-    /** Zoom constants */
-    Zoom.OFFSET = 80;
-    Zoom._MAX_WIDTH = 2560;
-    Zoom._MAX_HEIGHT = 4096;
+    /** AdvancedZoom constants */
+    AdvancedZoom.OFFSET = 80;
+    AdvancedZoom._MAX_WIDTH = 2560;
+    AdvancedZoom._MAX_HEIGHT = 4096;
 
     /**
-     * ZOOM ZOOMEDIA
+     * ADVANCEDZOOM ADVANCEDZOOMEDIA
      * @description Crete DOM for zoommed media
      * @param string tagName The tag name
      */
-    Zoom.prototype.zoomMedia = function(tagName) {
+    AdvancedZoom.prototype.advancedZoomMedia = function (tagName) {
         var target = this._targetMedia;
         var src = target.getAttribute('data-zoom-original') || target.currentSrc || target.src;
 
         target.classList.add('zoom-media-loading');
 
-        /** Zoom an IMG */
+        /** AdvancedZoom an IMG */
         if (tagName === 'IMG' || tagName === 'PICTURE') {
             var img = document.createElement('img');
 
-            img.onload = function() {
+            img.onload = function () {
                 this._fullHeight = Number(img.height);
                 this._fullWidth = Number(img.width);
-                this._zoomOriginal();
+                this._advancedZoomOriginal();
                 target.classList.remove('zoom-media-loading');
                 target.removeAttribute('data-zoom-original');
             }.bind(this);
@@ -249,17 +249,18 @@
             /** Set new source in overlay and target image */
             img.src = src;
 
-        /** Zoom a VIDEO */
-        } else if (tagName === 'VIDEO') {
+            /** AdvancedZoom a VIDEO */
+        }
+        else if (tagName === 'VIDEO') {
             var video = document.createElement('video');
             var source = document.createElement('source');
             var _this = this;
 
             video.appendChild(source);
-            video.addEventListener('canplay', function() {
+            video.addEventListener('canplay', function () {
                 _this._fullHeight = Number(video.videoHeight);
                 _this._fullWidth = Number(video.videoWidth);
-                _this._zoomOriginal();
+                _this._advancedZoomOriginal();
                 _this._targetMedia.play();
                 _this._targetMedia.classList.remove('zoom-media-loading');
             }, false);
@@ -273,10 +274,10 @@
     };
 
     /**
-     * ZOOM _ZOOMORIGINAL
+     * ADVANCEDZOOM _ADVANCEDZOOMORIGINAL
      * @description Create zoom DOM
      */
-    Zoom.prototype._zoomOriginal = function() {
+    AdvancedZoom.prototype._advancedZoomOriginal = function () {
         /** Media and Media Wrap */
         this._targetMediaWrap = document.createElement('div');
         this._targetMediaWrap.className = 'zoom-media-wrap';
@@ -291,15 +292,15 @@
         document.body.appendChild(this._overlay);
 
         /** Style Media DOM */
-        this._calculateZoom();
+        this._calculateAdvancedZoom();
         this._triggerAnimation();
     };
 
     /**
-     * ZOOM _CALCULATEZOOM
-     * @description Calcualate tha scale zoom
+     * ADVANCEDZOOM _CALCULATEADVANCEDZOOM
+     * @description Calcualate the scale zoom
      */
-    Zoom.prototype._calculateZoom = function() {
+    AdvancedZoom.prototype._calculateAdvancedZoom = function () {
         /** Repaint */
         this._targetMedia.offsetWidth; // jshint ignore:line
 
@@ -307,8 +308,8 @@
         var originalFullMediaHeight = this._fullHeight;
         var scrollTop = window.scrollY;
         var maxScaleFactor = originalFullMediaWidth / (this._targetMedia.width || this._targetMedia.videoWidth);
-        var viewportHeight = (window.innerHeight - Zoom.OFFSET);
-        var viewportWidth = (window.innerWidth - Zoom.OFFSET);
+        var viewportHeight = (window.innerHeight - AdvancedZoom.OFFSET);
+        var viewportWidth = (window.innerWidth - AdvancedZoom.OFFSET);
         var mediaAspectRatio = originalFullMediaWidth / originalFullMediaHeight;
         var viewportAspectRatio = viewportWidth / viewportHeight;
 
@@ -321,10 +322,10 @@
     };
 
     /**
-     * ZOOM _TRIGGERANIMATION
+     * ADVANCEDZOOM _TRIGGERANIMATION
      * @description Assign styles
      */
-    Zoom.prototype._triggerAnimation = function() {
+    AdvancedZoom.prototype._triggerAnimation = function () {
         /** Repaint */
         this._targetMedia.offsetWidth; // jshint ignore:line
 
@@ -350,10 +351,10 @@
     };
 
     /**
-     * ZOOM CLOSE
+     * ADVANCEDZOOM CLOSE
      * @description Close the zoom element
      */
-    Zoom.prototype.close = function() {
+    AdvancedZoom.prototype.close = function () {
         /** Remove class */
         this._body.classList.remove('zoom-overlay-open');
         this._body.classList.add('zoom-overlay-transitioning');
@@ -367,10 +368,10 @@
     };
 
     /**
-     * ZOOM DISPOSE
+     * ADVANCEDZOOM DISPOSE
      * @description Reset all
      */
-    Zoom.prototype.dispose = function() {
+    AdvancedZoom.prototype.dispose = function () {
         /** If zoommed elements exist reset all */
         if (this._targetMediaWrap && this._targetMediaWrap.parentNode) {
             this._targetMedia.classList.remove('zoom-media');
@@ -385,7 +386,7 @@
         }
     };
 
-    /** START ZOOM SERVICE LISTENING */
-    new ZoomService().listen();
+    /** START ADVANCEDZOOM SERVICE LISTENING */
+    new AdvancedZoomService().listen();
 
 }(); // jshint ignore:line
